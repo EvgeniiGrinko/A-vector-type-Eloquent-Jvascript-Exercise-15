eb1fe54ae8fed4f503427c4a50d22352f42ac15d
Evgenii <jenek0907@mail.ru>
2020-06-21 12:32:17 +0300

    Add files via upload

--- avectortype.js
+++ avectortype.js
@@ -1,20 +1,18 @@
 class Vec {
-    constructor(x,y,a,b){
+    constructor(x,y,){
         this.x = x;
         this.y = y;
        
     }
-    get sum (other){
-        return [(this.x+other.a) , (this.y+other.b)]
+    sum (other){
+        return new Vec (this.x+other.x,this.y+other.y);
     }
-    get minus(other){
-        return [(this.x-other.a), (this.y-other.b)]
+    minus(other){
+        return new Vec(this.x-other.x, this.y-other.y);
     }
     get length(){return Math.sqrt((this.x*this.x+this.y*this.y));
 
     }
 }
 let i = new Vec(23,45);
-console.log(i.length, i.minus(90,45), i.sum(57,92));
-let au = new Vec(231,567);
-console.log(au);
+console.log(i.length, new Vec(12,73).minus(new Vec(28,101)), new Vec(77,88).sum(new Vec(56,73)) );
